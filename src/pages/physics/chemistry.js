95f48ee435b2127de3508e2eeb9b16bf98c34e47
the-beatle <marcae7@gmail.com>
2022-08-24 15:18:16 -0500

    initial routes

---
+++ src/pages/physics/chemistry.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Chemistry() {
+    return (
+        <div style={{maxWidth:"800px",textAlign:"justify"}}>
+            <h2>Nuestros científicos están trabajando en esto.</h2>
+        </div>
+    )
+}
+
+export default Chemistry;
--- src/pages/physics/fluidMechanics.js
+++ src/pages/physics/fluidMechanics.js
@@ -8,8 +8,6 @@ function FluidMechanics() {
                 "Aquel que lo intentó y no lo consiguió es superior al que ni lo intentó.. Isaac Newton"</p>
             <h4 style={{textAlign:"left"}}>Laboratorio #2. Mecánica de fluidos</h4>
             <p> <strong>Palabras clave: </strong>Densidad, masa, velocidad, presión, caudal. </p>
-            <h4>Introducción</h4>
-            <p>Se denomina caída libre al movimiento de un cuerpo bajo la acción exclusiva de un campo gravitatorio.</p>
         </div>
     )
 }
