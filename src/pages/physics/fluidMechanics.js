abd4e3b1cae2ab0f331c962dccebc6d91396c559
nubenunca <marylinrestrepp@gmail.com>
2022-08-28 18:06:10 -0500

    título y definición

--- src/pages/physics/fluidMechanics.js
+++ src/pages/physics/fluidMechanics.js
@@ -5,9 +5,11 @@ function FluidMechanics() {
         <div style={{maxWidth:"800px",textAlign:"justify"}}>
             <h2>Semillero de ciencias</h2>
             <p style={{fontStyle:"italic", textAlign:"center"}}>
-                "Aquel que lo intentó y no lo consiguió es superior al que ni lo intentó.. Isaac Newton"</p>
+                "Aquel que lo intentó y no lo consiguió es superior al que ni lo intentó." -Isaac Newton</p>
             <h4 style={{textAlign:"left"}}>Laboratorio #2. Mecánica de fluidos</h4>
             <p> <strong>Palabras clave: </strong>Densidad, masa, velocidad, presión, caudal. </p>
+            <h3> Densidad </h3>
+            <p> Relación que existe entre la masa de un cuerpo y su volumen</p>
         </div>
     )
 }
