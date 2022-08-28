51b0c28bddb23c5b526b68a72043cc3ad700cda2
nubenunca <marylinrestrepp@gmail.com>
2022-08-28 18:18:42 -0500

    Agregué video

--- src/pages/physics/fluidMechanics.js
+++ src/pages/physics/fluidMechanics.js
@@ -1,6 +1,9 @@
 import React from "react";
+import YoutubeEmbed from "../../components/YoutubeEmbed";
+
 
 function FluidMechanics() {
+    const videoHeight  = "400px"
     return (
         <div style={{maxWidth:"800px",textAlign:"justify"}}>
             <h2>Semillero de ciencias</h2>
@@ -9,7 +12,9 @@ function FluidMechanics() {
             <h4 style={{textAlign:"left"}}>Laboratorio #2. Mecánica de fluidos</h4>
             <p> <strong>Palabras clave: </strong>Densidad, masa, velocidad, presión, caudal. </p>
             <h3> Densidad </h3>
-            <p> Relación que existe entre la masa de un cuerpo y su volumen</p>
+            <p > Relación que existe entre la masa de un cuerpo y su volumen. Su fórmula es: <strong>densidad = masa/volumen</strong></p>
+            <p style={{fontSize:"20px"}}> Observa en este video cómo es posible calcular la densidad de los cuerpos:</p>
+            <YoutubeEmbed embedId={"sy1_rVAzZBU"} height={videoHeight}/>
         </div>
     )
 }
