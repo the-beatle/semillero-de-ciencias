506d3a553b585d7b51d5f874656c31f11e0590cf
the-beatle <marcae7@gmail.com>
2022-08-28 19:05:21 -0500

    Add initial multiple choice component

---
+++ src/components/SingleChoiceQuiz.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SingleChoiceQuiz = ({src})=>{
+    return(
+        <div>
+            <div>Representa la cantidad de materia que tienen los cuerpos</div>
+            <ol>
+                <li>Masa</li>
+                <li>Volumen</li>
+                <li>Gravedad</li>
+                <li>Peso</li>
+            </ol>
+        </div>
+    )
+}
+
+export default SingleChoiceQuiz;
