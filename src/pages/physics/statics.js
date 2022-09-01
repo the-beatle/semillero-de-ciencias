36083552417c0b2e3725f77a81e165bce985b086
the-beatle <marcae7@gmail.com>
2022-09-01 16:53:04 -0500

    Módulo inicial de estática

---
+++ src/pages/physics/statics.js
@@ -0,0 +1,16 @@
+import React, {useEffect, useMemo, useState} from "react";
+
+
+function Statics() {
+
+    return (
+        <div style={{maxWidth:"800px",textAlign:"justify"}}>
+            <h2>Semillero de ciencias</h2>
+            <p style={{fontStyle:"italic", textAlign:"center"}}>
+                "Eureka!" -Arquímedes</p>
+            <h4 style={{textAlign:"left"}}> Estática</h4>
+        </div>
+    )
+}
+
+export default Statics;
